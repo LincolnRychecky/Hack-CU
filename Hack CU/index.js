beee6265e68856a830ab16f8381b3b40bcd8589e
Raegan Rychecky <rary4207@colorado.edu>
2020-02-22 23:10:31 -0700

    Updates

--- Hack CU/index.js
+++ Hack CU/index.js
@@ -1,61 +1,70 @@
-//   // Your web app's Firebase configuration
-//   var firebaseConfig = {
-//     apiKey: "AIzaSyDZkphKGDOGFqEShcoySi39tFB7BtAR0CY",
-//     authDomain: "invasive-species-reporter.firebaseapp.com",
-//     databaseURL: "https://invasive-species-reporter.firebaseio.com",
-//     projectId: "invasive-species-reporter",
-//     storageBucket: "invasive-species-reporter.appspot.com",
-//     messagingSenderId: "198911272608",
-//     appId: "1:198911272608:web:595d186746e2b1f94ccbc5",
-//     measurementId: "G-4571BBW3QX"
-//   };
-//   // Initialize Firebase
-// firebase.initializeApp(firebaseConfig); //creating instance of firebase
-//   // Get a reference to the database
-// var database = firebase.firestore();
-// var docRef = database.collection("Aquatics Reports");
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDZkphKGDOGFqEShcoySi39tFB7BtAR0CY",
+    authDomain: "invasive-species-reporter.firebaseapp.com",
+    databaseURL: "https://invasive-species-reporter.firebaseio.com",
+    projectId: "invasive-species-reporter",
+    storageBucket: "invasive-species-reporter.appspot.com",
+    messagingSenderId: "198911272608",
+    appId: "1:198911272608:web:595d186746e2b1f94ccbc5",
+    measurementId: "G-4571BBW3QX"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig); //creating instance of firebase
+  // Get a reference to the database
+var database = firebase.firestore();
+var docRef = database.collection("Aquatics Reports");
 
+var selectedFish = "";
 
-function display_Name() {
-  document.getElementById("Species Name").innerHTML = "Raegan";
+function getNameFromClick(fishName){
+  selectedFish = fishName;
+  console.log(selectedFish);
 }
-function display_Description() {
-  document.getElementById("Species Description").innerHTML = "Raegan";
+function display_Name(name) {
+  document.getElementById("Species Name").innerHTML = name;
 }
-function display_Problem() {
-  document.getElementById("Problem").innerHTML = "Raegan";
+function display_Description(description) {
+  document.getElementById("Species Description").innerHTML = description;
 }
-function display_waterSheds() {
-  document.getElementById("Water Sheds").innerHTML = "Raegan";
+function display_Problem(problem) {
+  document.getElementById("Problem").innerHTML = problem;
+}
+function display_waterSheds(waterSheds) {
+  document.getElementById("Water Sheds").innerHTML = waterSheds;
 }
 
-function GFG_Fun() {
+function GFG_Fun(image) {
   var img = document.createElement("img");
-  img.src = "SMB.png";
+  img.src = image;
   var src = document.getElementById("header");
   src.appendChild(img);
 }
-//
-// function findFish(searchName)
-// {
-//
-//   database.collection("InvasiveSpeciesEntries").get().then(function(querySnapshot)
-//   {querySnapshot.forEach(function(doc){
-//     if(doc.exists && doc.data().name == searchName)
-//     {
-//       console.log("Document data:", doc.data());
-//       var post = doc.data();
-//       var name = post.name;
-//       var description = post.description;
-//       var problem = post.problem;
-//       var waterSheds = post.waterSheds;
-//
-//       display_Name("Raegan")
-//       // var card1 = addPosting(name, description, problem, waterSheds);
-//       // document.getElementById('speciesCard').appendChild(card1);
-//     }
-//
-//     });
-//   });
-//
-// }
+
+function findFish()
+{
+
+  database.collection("InvasiveSpeciesEntries").get().then(function(querySnapshot)
+  {querySnapshot.forEach(function(doc){
+    if(doc.exists && doc.data().name == selectedFish)
+    {
+      console.log("Document data:", doc.data());
+      var post = doc.data();
+      var name = post.name;
+      var description = post.description;
+      var problem = post.problem;
+      var waterSheds = post.waterSheds;
+      var image = post.image;
+
+      display_Name(name);
+      display_Description(description);
+      display_Problem(problem);
+      display_waterSheds(waterSheds);
+      GFG_Fun(image);
+
+    }
+
+    });
+  });
+
+}
