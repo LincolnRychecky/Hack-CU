4df9f78345f052bc7184980b6e4e63caf22658c1
Lincoln Rychecky <liry9073@colorado.edu>
2020-02-22 17:24:27 -0700

    Changes

--- Hack CU/index.js
+++ Hack CU/index.js
@@ -1,13 +1,3 @@
 var database = firebase.firestore();
 
 var docRef = database.collection("Aquatics Reports");
-
-$(function () {
-    $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > 10 ) {
-            $('.navbar').addClass('active');
-        } else {
-            $('.navbar').removeClass('active');
-        }
-    });
-});
