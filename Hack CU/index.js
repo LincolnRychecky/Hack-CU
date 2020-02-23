b0f60f4cabf770e3203bd883e6aab5273c37f1c5
Raegan Rychecky <rary4207@colorado.edu>
2020-02-22 22:26:04 -0700

    Dynamic Upload From Database

--- Hack CU/index.js
+++ Hack CU/index.js
@@ -1,68 +1,47 @@
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyDZkphKGDOGFqEShcoySi39tFB7BtAR0CY",
-    authDomain: "invasive-species-reporter.firebaseapp.com",
-    databaseURL: "https://invasive-species-reporter.firebaseio.com",
-    projectId: "invasive-species-reporter",
-    storageBucket: "invasive-species-reporter.appspot.com",
-    messagingSenderId: "198911272608",
-    appId: "1:198911272608:web:595d186746e2b1f94ccbc5",
-    measurementId: "G-4571BBW3QX"
-  };
-  // Initialize Firebase
-firebase.initializeApp(firebaseConfig); //creating instance of firebase
-  // Get a reference to the database
-var database = firebase.firestore();
-var docRef = database.collection("Aquatics Reports");
+//   // Your web app's Firebase configuration
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyDZkphKGDOGFqEShcoySi39tFB7BtAR0CY",
+//     authDomain: "invasive-species-reporter.firebaseapp.com",
+//     databaseURL: "https://invasive-species-reporter.firebaseio.com",
+//     projectId: "invasive-species-reporter",
+//     storageBucket: "invasive-species-reporter.appspot.com",
+//     messagingSenderId: "198911272608",
+//     appId: "1:198911272608:web:595d186746e2b1f94ccbc5",
+//     measurementId: "G-4571BBW3QX"
+//   };
+//   // Initialize Firebase
+// firebase.initializeApp(firebaseConfig); //creating instance of firebase
+//   // Get a reference to the database
+// var database = firebase.firestore();
+// var docRef = database.collection("Aquatics Reports");
 
 
-function addEvent()
-{
-  console.log("Hello")
-  //ADD A header
-  var headTop = document.createElement("h1");
-  headTop.innerHTML = "smallmouth Bass";
-  document.getElementById("speciesCard").appendChild(headTop);
-
-  // //Create card
-  // let card = document.createElement("div");
-  // card.className = 'card';
-  //
-  // //Add card body
-  // let cardBody = document.createElement('div');
-  // card.appendChild(cardBody);
-  // cardBody.className = 'card-header';
-  //
-  // //Add title
-  // let cardTitle = document.createElement('h5');
-  // cardTitle.innerHTML = title;
-  // cardBody.appendChild(cardTitle);
-  //
-  // //Add description
-  // let desc = document.createElement('p');
-  // card.appendChild(desc);
-  // desc.innerText = description;
-  // cardBody.appendChild(desc);
-  //
-  // //Add user contact info
-  // // <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-  // //   Button with data-target
-  // // </button>
-  //
-  // let info = document.createElement('p');
-  // card.appendChild(info);
-  // info.innerText = contact;
-  // cardBody2.appendChild(info);
-  //
-  // return card;
+function display_Name() {
+  document.getElementById("Species Name").innerHTML = "Raegan";
+}
+function display_Description() {
+  document.getElementById("Species Description").innerHTML = "Raegan";
+}
+function display_Problem() {
+  document.getElementById("Problem").innerHTML = "Raegan";
+}
+function display_waterSheds() {
+  document.getElementById("Water Sheds").innerHTML = "Raegan";
 }
 
+function GFG_Fun() {
+  var img = document.createElement("img");
+  img.src = "SMB.png";
+  var src = document.getElementById("header");
+  src.appendChild(img);
+}
+//
 // function findFish(searchName)
 // {
 //
 //   database.collection("InvasiveSpeciesEntries").get().then(function(querySnapshot)
 //   {querySnapshot.forEach(function(doc){
-//     if(doc.exists && doc.data().name == )
+//     if(doc.exists && doc.data().name == searchName)
 //     {
 //       console.log("Document data:", doc.data());
 //       var post = doc.data();
@@ -70,8 +49,10 @@ function addEvent()
 //       var description = post.description;
 //       var problem = post.problem;
 //       var waterSheds = post.waterSheds;
-//       var card1 = addPosting(name, description, problem, waterSheds);
-//       document.getElementById('speciesCard').appendChild(card1);
+//
+//       display_Name("Raegan")
+//       // var card1 = addPosting(name, description, problem, waterSheds);
+//       // document.getElementById('speciesCard').appendChild(card1);
 //     }
 //
 //     });
